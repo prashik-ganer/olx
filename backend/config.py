ffca9ea40cdeb44b33c326d7ad71121172b5b59f
Prashik Ganer <prashik.ganer@gmail.com>
2024-08-17 22:59:51 +0530

    Revert "Merge pull request #3 from v-sanju/prashik"
    This reverts commit 0ea4cea4f05906edd416d7816301ab8b1f584a93, reversing
    changes made to 8d820612da657f2eee94215c4aff7dce389b3c16.

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-uri = "mongodb+srv://sanjivk241:Sanjeevk241@olx1.byrgdqf.mongodb.net/?retryWrites=true&w=majority&appName=OLX1"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-db = client.todo_db
-collections = db["users"]
